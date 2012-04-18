fe4598abeec21c039f20d897e76853a57d3f389b
Nicola Spesivtsev <nikolay.s@monterosa.co.uk>
2012-04-18 12:41:31 +0400

    Initial commit

---
+++ slideshow.php
@@ -0,0 +1,12 @@
+<?php include ("inc-header.php"); ?>
+			
+			</div> <!-- close #header -->
+			<div id="content">
+				<div id="full-image">
+					<div id="slideshowpage">
+						<?php printSlideShow(true,true); ?>
+					</div>
+				</div>
+			</div>
+
+<?php include ("inc-footer.php"); ?>	
---
+++ theme_description.php
@@ -0,0 +1,8 @@
+<?php
+// Zenphoto theme definition file
+$theme_description['name'] = 'zpminimal_v1.4.1a';
+$theme_description['author'] = 'gjr';
+$theme_description['version'] = '1.4.1a';
+$theme_description['date'] = '7/2/2011';
+$theme_description['desc'] = 'A ZenPhoto/ZenPage minimalistic theme.';
+?>
