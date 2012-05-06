75f37092f3f3e384d1e9395b93455546bf829c69
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2012-05-06 23:47:35 +0400

    * Customized 404 page

--- 404.php
+++ 404.php
@@ -1,35 +1,19 @@
 <?php include ("inc-header.php"); ?>
 
 			</div> <!-- close #header -->
-			<div id="content">
-				<div id="main"<?php if ($hrsi_switch) echo ' class="switch"'; ?>>
-					<div id="random-image">
-						<?php printRandomImages(1,null,'all','',190,225,true); ?>
-					</div>
-					<p><?php echo gettext("The page you are requesting cannot be found."); ?></p>
-					<h3>
-					<?php
-					if (isset($album)) {
-						echo '<br />'.sprintf(gettext('Album: %s'),sanitize($album));
-					}
-					if (isset($image)) {
-						echo '<br />'.sprintf(gettext('Image: %s'),sanitize($image));
-					}
-					if (isset($obj)) {
-						echo '<br />'.sprintf(gettext('Page: %s'),substr(basename($obj),0,-4));
-					}
-					?>
-					</h3><br />
-					<div id="enter">
-						<a href="<?php echo getCustomPageURL('gallery'); ?>" title="<?php echo gettext('Gallery index'); ?>"><?php echo gettext('Back to Gallery Index &rarr;'); ?></a>
-					</div>
-				</div>
-				<div id="sidebar"<?php if ($hrsi_switch) echo ' class="switch"'; ?>>
-					<div class="sidebar-divide">
-						<?php printGalleryDesc(true); ?>
-					</div>
+			<div id="content" class="grid_12">
+				<div id="sidebar" class="grid_3 alpha">
 					<?php include ("inc-sidemenu.php"); ?>
 				</div>
+				
+				<div class="grid_9 omega">
+					<p><?php echo gettext("The page you are requesting cannot be found."); ?></p>					
+				</div>
+
+
 			</div>
 
-<?php include ("inc-footer.php"); ?>			
+<?//php include ("inc-footer.php"); ?>			
+
+					
+			
