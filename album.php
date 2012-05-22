1356b5966f67881217c9831faf806873e9e26459
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2012-05-23 02:12:45 +0400

    * changed theme layout to use fixed height on album page

--- album.php
+++ album.php
@@ -10,7 +10,7 @@
 					<div class="scrollable">
 					<div id="scrollable-container" class="container">
 						<?php if (!(getAlbumDesc().length == 0)) { ?>
-							<div id="post" class="post grid_6 suffix_2 alpha">
+							<div id="post" class="post grid_7 suffix_1 alpha">
 								<h2 class="top"><?php printAlbumTitle(true); ?></h2>
 								<?php echo getAlbumDesc() ?>
 								</div>
@@ -19,17 +19,17 @@
 						<?php while (next_image()): ?>
 						<?php $image_number = $image_number + 1; ?>
 						
-						<div id="image-wrap-<?php echo $image_number; ?>" class="an-image suffix_2">
+						<div id="image-wrap-<?php echo $image_number; ?>" class="an-image suffix_1">
 						<?php if ((getAlbumDesc().length == 0)) { ?>
 							<div class="alpha image-nav grid_6 omega">
 								<span><?php $image_number == 1 ? printAlbumTitle(true) : "&nbsp;"; ?></span>
 							</div>
 						<?php } ?>						
 							<div id="full-image-<?php echo $image_number; ?>">
-								<?php if (($hrlfh_finallink)=='colorbox') { ?><a class="thickbox" href="<?php echo html_encode(getUnprotectedImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(),620); ?></a><?php } ?>
-								<?php if (($hrlfh_finallink)=='nolink') { printCustomSizedImage(getAnnotatedImageTitle(),620); } ?>
-								<?php if (($hrlfh_finallink)=='standard') { ?><a href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(),620); ?></a><?php } ?>
-								<?php if (($hrlfh_finallink)=='standard-new') { ?><a target="_blank" href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(),620); ?></a><?php } ?>
+								<?php if (($hrlfh_finallink)=='colorbox') { ?><a class="thickbox" href="<?php echo html_encode(getUnprotectedImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 620); ?></a><?php } ?>
+								<?php if (($hrlfh_finallink)=='nolink') { printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 620); } ?>
+								<?php if (($hrlfh_finallink)=='standard') { ?><a href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 620); ?></a><?php } ?>
+								<?php if (($hrlfh_finallink)=='standard-new') { ?><a target="_blank" href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 620); ?></a><?php } ?>
 							</div>
 
 						</div>
