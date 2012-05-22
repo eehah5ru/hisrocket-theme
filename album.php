49627150501b0dd4f3140d0cc4e0500dfe21cc7b
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2012-05-22 23:40:26 +0400

    * added skipping album description if it is empty

--- album.php
+++ album.php
@@ -9,15 +9,22 @@
 				<div id="thumbs-wrap" class="grid_9 omega">
 					<div class="scrollable">
 					<div id="scrollable-container" class="container">
-
-						<div id="post" class="post grid_6 suffix_2 alpha">
-							<h2 class="top"><?php printAlbumTitle(true); ?></h2>
-							<?php echo getAlbumDesc() ?>
-						</div>
+						<?php if (!(getAlbumDesc().length == 0)) { ?>
+							<div id="post" class="post grid_6 suffix_2 alpha">
+								<h2 class="top"><?php printAlbumTitle(true); ?></h2>
+								<?php echo getAlbumDesc() ?>
+								</div>
+						<?php } ?>
 						<?php $image_number = 0; ?>
 						<?php while (next_image()): ?>
 						<?php $image_number = $image_number + 1; ?>
+						
 						<div id="image-wrap-<?php echo $image_number; ?>" class="an-image suffix_2">
+						<?php if ((getAlbumDesc().length == 0)) { ?>
+							<div class="alpha image-nav grid_6 omega">
+								<span><?php $image_number == 1 ? printAlbumTitle(true) : "&nbsp;"; ?></span>
+							</div>
+						<?php } ?>						
 							<div id="full-image-<?php echo $image_number; ?>">
 								<?php if (($hrl_finallink)=='colorbox') { ?><a class="thickbox" href="<?php echo html_encode(getUnprotectedImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(),620); ?></a><?php } ?>
 								<?php if (($hrl_finallink)=='nolink') { printCustomSizedImage(getAnnotatedImageTitle(),620); } ?>
