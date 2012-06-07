bddcc12d798f6301c2d9cff2fad691a425d3ff2e
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2012-06-08 02:04:52 +0400

    * removed image titles

--- album.php
+++ album.php
@@ -26,7 +26,7 @@
 						<?php } ?>						
 							<div id="full-image-<?php echo $image_number; ?>">
 								<?php if (($hrlfh_finallink)=='colorbox') { ?><a class="thickbox" href="<?php echo html_encode(getUnprotectedImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 560); ?></a><?php } ?>
-								<?php if (($hrlfh_finallink)=='nolink') { printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 560); } ?>
+								<?php if (($hrlfh_finallink)=='nolink') { printCustomSizedImage(NULL, NULL, NULL, 560); } ?>
 								<?php if (($hrlfh_finallink)=='standard') { ?><a href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 560); ?></a><?php } ?>
 								<?php if (($hrlfh_finallink)=='standard-new') { ?><a target="_blank" href="<?php echo html_encode(getFullImageURL());?>" title="<?php echo getBareImageTitle();?>"><?php printCustomSizedImage(getAnnotatedImageTitle(), NULL, NULL, 560); ?></a><?php } ?>
 							</div>
